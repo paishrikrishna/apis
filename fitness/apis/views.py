f1022f460b98e6ee40c3db4053dd01b8099acf24
paishrikrishna <43004882+paishrikrishna@users.noreply.github.com>
2022-05-17 00:26:26 +0530

    Update views.py

--- fitness/apis/views.py
+++ fitness/apis/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 import math, random
 from apis.firebase_calls import firebase_data_api
 import boto3
-s = smtplib.SMTP('smtp.gmail.com',465)
+s = smtplib.SMTP('smtp.gmail.com',587)
 
 
 
